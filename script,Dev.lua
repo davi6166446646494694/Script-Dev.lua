a59bc0cd3f8c280219fb4e61e59cc28aefd66086
Goku <2204mariadejesus1949@gmail.com>
2025-12-15 14:52:36 -0300

    Add additional script loading from GitHub

--- script,Dev.lua
+++ script,Dev.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/davi6166446646494694/Up/refs/heads/main/obf_L8K7oXWoUIvQvn1maOi6MNch6xEyfNKBQy2dsqiiFPjV93j7ZMu1SKp3Jyw0Ev3w.lua.txt"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/davi6166446646494694/D/refs/heads/main/obf_gd6yfcYPow2F9I058Av9102ly0YOI59Ab380wRrr3Mi01YJznAKUmUa01h7s2BXD.lua.txt"))()
