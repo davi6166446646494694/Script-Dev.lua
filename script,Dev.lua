1e8bfe64a02e2f227e83606f76e0ad11982abd6e
Goku <2204mariadejesus1949@gmail.com>
2025-12-17 19:37:55 -0300

    Update script to load new version of Lua file

--- script,Dev.lua
+++ script,Dev.lua
@@ -1,2 +1,1 @@
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/davi6166446646494694/D/refs/heads/main/obf_gd6yfcYPow2F9I058Av9102ly0YOI59Ab380wRrr3Mi01YJznAKUmUa01h7s2BXD.lua.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/davi6166446646494694/Vers-o-4.0/main/obf_n44QHZ8S3Zg0sPDUn0MXPuq32GT5L8LLatmJ6O6vw6mh8E4boR13KZS1Ai29k0wS.lua.txt"))()
