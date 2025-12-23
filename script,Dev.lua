a61e81860045ff9cbc09865c05c00a040a5e476f
Goku <2204mariadejesus1949@gmail.com>
2025-12-23 04:44:49 -0300

    Update script,Dev.lua

--- script,Dev.lua
+++ script,Dev.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/davi6166446646494694/Script-Dev.lua/main/VERS%C3%83O%205.0%20%281%29.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/davi6166446646494694/Script-Dev.lua/refs/heads/main/obfuscated_script-1766475667398.lua.txt"))()
