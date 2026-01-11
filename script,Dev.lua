991feec1b078cadb4cc7f55bc994ba8aa2da2ca3
Goku <2204mariadejesus1949@gmail.com>
2026-01-10 23:25:23 -0300

    Update script,Dev.lua

--- script,Dev.lua
+++ script,Dev.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/davi6166446646494694/Vers-o-7/refs/heads/main/vers%C3%A3o%207.txt"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/davi6166446646494694/Script-Dev.lua/refs/heads/main/Animação"))()
+lua
